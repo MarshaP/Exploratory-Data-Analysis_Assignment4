cc9cc17c9bfcce672493a30d3a515c8cff3ba57d
MarshaP <mpmarpac@gmail.com>
2017-11-11 22:01:56 -0600

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,33 @@
+if (!file.exists("Exploratory_Data_Analysis")){
+  dir.create("Exploratory_Data_Analysis")
+}
+
+setwd("C:\\Users\\mpmar\\Desktop\\data scientists_courses\\Exploratory_Data_Analysis")
+
+
+
+##load the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Subset data by Baltimore's fip.
+NEIdataBaltimore<-subset(NEI, fips == "24510")
+
+# Aggregate: sum the emissions in Baltimore data by year
+aggTotalsBaltimore <- aggregate(Emissions ~ year, NEIdataBaltimore,sum)
+
+#generate graph
+png("plot3.png", width=480, height=480)
+
+library(ggplot2)
+
+ggp <- ggplot(NEIdataBaltimore,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
